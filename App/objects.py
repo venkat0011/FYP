efe46f01701a54eb84ddad62ea09d6519663c38d
venklat0011 <venkat.s81298@gmail.com>
2023-02-10 23:17:33 +0800

    Added Image Button for Schema Selection Page

--- App/objects.py
+++ App/objects.py
@@ -1,7 +1,7 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QGridLayout, QWidget
-from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QGridLayout, QWidget, QVBoxLayout
+from PyQt5.QtGui import QFont, QPixmap, QIcon
 from PyQt5 import QtCore
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QSize
 import matlab.engine
 import threading
 from functools import partial
@@ -30,12 +30,12 @@ class MainWindow(QMainWindow):
 
         # add the label
         label = QLabel("Occupancy Sensing Using RF sensing", self)
-        label.setFont(QFont("Arial", 20))
+        label.setFont(QFont("Arial", 15))
         self.main_layout.addWidget(label, 0, 0, 1, 1, Qt.AlignCenter)
 
         # progress label
         self.progress_label = QLabel("Please wait, the app is connecting to MATLAB", self)
-        self.progress_label.setFont(QFont("Arial", 15))
+        self.progress_label.setFont(QFont("Arial", 12))
         self.progress_label.setContentsMargins(10, 10, 10, 10)
         self.main_layout.addWidget(self.progress_label, 1, 0, 1, 1, Qt.AlignCenter)
 
@@ -47,20 +47,24 @@ class MainWindow(QMainWindow):
         # self.stop_event = threading.Event()
     def on_matlab_started(self):
         # add the schema selection label
+
         self.progress_label.setText("Please select the type of RF sensor to be used")
         #
         # # add the Ir-uwb button
-        ir_uwb_button = QPushButton("Ir-uwb", self)
+        ir_uwb_button = QPushButton("IR-UWB", self)
+        ir_uwb_button.setFont(QFont('Arial',11))
         ir_uwb_button.clicked.connect(self.display_schema_choice)
         ir_uwb_button.setContentsMargins(10, 10, 10, 10)
         self.main_layout.addWidget(ir_uwb_button, 3, 0, 1, 1, Qt.AlignCenter)
         #
         # # add the FMCW button
         fmcw_button = QPushButton("FMCW", self)
+        fmcw_button.setFont(QFont('Arial', 11))
         fmcw_button.clicked.connect(self.display_schema_choice)
-        fmcw_button.setContentsMargins(10, 10, 10, 10)
+        fmcw_button.setContentsMargins(10, 15, 10, 10)
         self.main_layout.addWidget(fmcw_button, 4, 0, 1, 1, Qt.AlignCenter)
 
+        self.main_layout.setRowMinimumHeight(3, 50 )
     def display_schema_choice(self):
         # implement the display_schema_choice function
         self.schema_selection_window = SchemaSelection()
@@ -85,10 +89,50 @@ class SchemaSelection(QMainWindow):
         central_widget.setLayout(main_layout)
 
         # add the label
-        label = QLabel("Choose a schema", self)
-        label.setFont(QFont("Arial", 20))
-        main_layout.addWidget(label)
+        label = QLabel("Please select the orientation of the seats", self)
+        label.setFont(QFont("Arial", 15))
+        label.setContentsMargins(10, 10, 10, 10)
+        main_layout.addWidget(label, 0, 1, 1, 1, Qt.AlignCenter)
+        #
+        home_page_button = QPushButton("Home Page", self)
+        home_page_button.clicked.connect(self.display_home)
+        home_page_button.setContentsMargins(10, 10, 10, 10)
+        main_layout.addWidget(home_page_button, 0, 0, 1, 1, Qt.AlignCenter)
 
+        # creating the image button for the 3 different orientation
+        y_button = QPushButton(self)
+        y_button.setMaximumSize(65, 270)
+        # Load an image from file
+        pixmap = QPixmap("y_direction.png")
+        button_icon = QIcon(pixmap)
+        y_button.setIcon(button_icon)
+        y_button.setIconSize(QSize(60, 266))
+        main_layout.addWidget(y_button, 1, 0, 1, 1, Qt.AlignCenter)
+
+        # label for the button
+        y_label = QLabel("Y Direction", self)
+        y_label.setFont(QFont('Arial', 10))
+        main_layout.addWidget(y_label, 2, 0, 1, 1, Qt.AlignCenter)
+
+    def display_home(self):
+        home_page = MainWindow()
+        home_page.show()
+        self.hide()
+
+    def display_y_schema(self):
+        print('hello')
+
+
+
+    # putting the button in its place
+    # by using grid
+    # button1.grid(row=0, column=0, padx=0, pady=0)
+    # window_count = len(new_window.children)
+    # print("before entry page ",window_count)
+    # # # display the 3 different orientation
+    # Button(Frame, "Y Direction", lambda: get_coordinates(0, new_window,window_count), (60, 266), 1, 0, "y_direction.png", 20, 20)
+    # Button(Frame, "X Direction", lambda: get_coordinates(1, new_window,window_count), (300, 40), 1, 1, "x_direction.png", 35, 140)
+    # Button(Frame, "2 Dimension", lambda: get_coordinates(2, new_window,window_count), (106, 200), 1, 2, "2d_space.png", 30, 50)
 
 # create the application
 app = QApplication([])
